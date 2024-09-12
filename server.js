8d929ea9c55eaeb3f0c389b48fd7a970fb05a7d4
Bradyn Wolkoys <bnwolkoys@gmail.com>
2024-09-12 11:14:35 -0700

    added app.delete and added route to html

--- server.js
+++ server.js
@@ -24,3 +24,28 @@ app.post('/api/notes', (req, res) => {
     fs.writeFileSync('./db/db.json', JSON.stringify(db))
     res.json(db)
 })
+
+//Delete
+app.delete('/api/notes/:id', (req, res) => {
+    const newDb = db.filter((note) => 
+    note.id !== req.params.id)
+    fs.writeFileSync('./db/db.json', JSON.stringify(newDb))
+    readFile.json(newDb)
+});
+
+const PORT = process.env.PORT || 3000
+
+// routes (html)
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'))
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+});
+
+app.listen(PORT, () => console.log(`App is listening on ${PORT}`));
